4c93a89d9413dd5d60520d06eaec112eda57d834
janobboy <qulamatovjanob@gmail.com>
2023-11-28 14:39:01 +0500

    code

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,14 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
-    
+    i=0
+    mx   = data[0]
+    while i<len(data):
+        x = data[i]
+
+        if x > mx:
+            mx = x
+        i+=1
+    return mx
+print(find_max([1, 2, 3, 4, 5]))
+print(find_max([12, 2, 5, 2, 7, 9, 1]))
